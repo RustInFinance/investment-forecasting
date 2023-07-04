d811225c4955b195fa2ab9fd6a0762b21b44e2aa
Jacek Czaja <jacek.czaja@intel.com>
2023-07-04 10:17:54 +0200

    - added more stats

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,30 @@ fn generate_revolut_gains(capital : f32, investment_rate : f32,time_line : &Vec<
     (curr_capital,gains)
 }
 
+fn generate_obligacje_gains(capital : f32, investment_rate : f32,time_line : &Vec<u32>) -> (f32,Vec<f32>)
+{
+
+    let mut gains : Vec<f32> = vec![];
+    let mut curr_capital = capital;
+    let mut curr_gain = 0.0f32;
+
+    time_line.iter().for_each(|x| {
+        let mut g : f32 = 0.0f32; 
+        if x % 365 == 0 {
+            (curr_capital,g) = compute_gain(curr_capital, investment_rate, 1, 0.0); 
+        }
+        curr_gain+=g;
+        gains.push(curr_gain as f32);
+    });
+    let ng = match gains.pop() {
+       Some(g) => g*(1.0-0.19),
+       None => panic!("This is wrong!"),
+    };
+    gains.push(ng);
+
+    (curr_capital,gains)
+}
+
 fn generate_santander_gains(capital : f32, investment_rate : f32,time_line : &Vec<u32>) -> (f32,Vec<f32>)
 {
     let mut gains : Vec<f32> = vec![];
@@ -62,13 +86,17 @@ fn main() {
 
     // Data of Revolut
     let (total_revolut, revolut_gains) = generate_revolut_gains(base_capital,0.0405,&time_data);
+    let (total_obligacje, obligacje_gains) = generate_revolut_gains(base_capital,0.075,&time_data);
     // Data of santander
     let (total_santander, santander_gains) = generate_santander_gains(base_capital,0.04,&time_data);
+    let (total_toyota, toyota_gains) = generate_santander_gains(base_capital,0.07,&time_data);
     let max_range = if total_santander > total_revolut { (total_santander-base_capital) } else { (total_revolut-base_capital)}; 
 
 
     let revolut_caption = format!("Revolut (Total: {}, Gains: {})",total_revolut,(total_revolut-base_capital));
     let santander_caption = format!("Santander (Total: {}, Gains: {})",total_santander,(total_santander-base_capital));
+    let toyota_caption = format!("Toyota (Total: {}, Gains: {})",total_toyota,(total_toyota-base_capital));
+    let obligacje_caption = format!("Obligacje (Total: {}, Gains: {})",total_obligacje,(total_obligacje-base_capital));
 
     // make actual plot
     let colors: Vec<&str> = vec!["blue", "green", "navy", "web-green"];
@@ -88,7 +116,7 @@ fn main() {
         )
         .set_y_range(
             gnuplot::AutoOption::Fix(0.0),
-            gnuplot::AutoOption::Fix( max_range as f64 *2.0 as f64), 
+            gnuplot::AutoOption::Fix( max_range as f64 *3.0 as f64), 
         );
         
         axes.lines(
@@ -100,6 +128,16 @@ fn main() {
             &time_data,
             &santander_gains,
             &[Caption(&santander_caption), Color(colors[1])],
+        );
+        axes.lines(
+            &time_data,
+            &toyota_gains,
+            &[Caption(&toyota_caption), Color(colors[2])],
+        );
+        axes.lines(
+            &time_data,
+            &obligacje_gains,
+            &[Caption(&obligacje_caption), Color(colors[3])],
         );
     fg.show().expect("Error plotting");
 
