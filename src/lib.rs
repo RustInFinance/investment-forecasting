23536d61e0d4d94c586ea589758ab52369f6c14f
Jacek Czaja <jacek.czaja@intel.com>
2024-03-28 13:02:54 +0100

    - implementing DGR

--- src/lib.rs
+++ src/lib.rs
@@ -341,7 +341,7 @@ async fn get_dividiend_data(
         panic!("No dividend Data!");
     };
 
-    let dgr = calculate_dgr(&div_history, frequency)?;
+    let dgr = calculate_dgr(&div_history, Utc::now().year().to_string().as_ref())?;
     log::info!("Current Div: {curr_div} {currency}, Paide date: {curr_div_date},  Frequency: {frequency}, Average DGR(samples: {}): {dgr}",
             div_history.len());
 
@@ -736,41 +736,64 @@ fn calculate_divy(
 }
 
 /// DGR On quaterly basis calculate
-fn calculate_dgr(div_history: &Vec<(String, f64)>, frequency: u32) -> Result<f64, &'static str> {
+fn calculate_dgr(
+    div_history: &Vec<(String, f64)>,
+    current_year: &str,
+) -> Result<f64, &'static str> {
     let dhiter = div_history.iter();
 
     let mut average = 0.0;
-    let mut count: u32 = 0;
-    let mut prev_val = 0.0;
     let mut annual_div = 0.0;
 
-    // If there are some dividend data but frequency is zero then
-    // comapny seemed to stop paying dividends
-    if frequency == 0 {
-        return Ok(0.0);
-    }
+    let current_year = current_year
+        .parse::<i32>()
+        .expect("Unable to parse currrent year");
+    let mut annual_div: BTreeMap<i32, f64> = BTreeMap::new();
 
-    dhiter.for_each(|(_, new_val)| {
-        count += 1;
-        annual_div += new_val;
-        if (count % frequency) == 0 {
-            if prev_val == 0.0 {
-                average = 0.0;
-            } else {
-                average += (annual_div / prev_val - 1.0) * 100.0;
+    div_history.iter().try_for_each(|x| {
+        let year = NaiveDate::parse_from_str(&x.0, "%Y-%m-%d")
+            .map_err(|_| "Error parsing dividend year")?
+            .year();
+        // Skip current year (no full data yet)
+        if year != current_year {
+            let possible_sum = annual_div.get_mut(&year);
+            match possible_sum {
+                Some(s) => *s += x.1,
+                None => {
+                    annual_div.insert(year, x.1);
+                    ()
+                }
             }
-            prev_val = annual_div;
-            annual_div = 0.0;
         }
-    });
-    count /= frequency;
-    count -= 1;
+        Ok::<(), &str>(())
+    })?;
 
-    if count == 0 {
-        Ok(0.0)
-    } else {
-        Ok(average / count as f64)
+    // Update data with zeros when there was no dividends in a given historical period
+    let oldest_year = annual_div
+        .iter()
+        .next()
+        .ok_or("Error: unable to get devidend")?
+        .0;
+    for y in *oldest_year..current_year {
+        if annual_div.contains_key(&y) == false {
+            annual_div.insert(y, 0.0);
+            log::info!("Company was having a gap in paying divdends at {y}");
+        }
     }
+
+    let mut from_newer_to_older = annual_div.iter().rev();
+    let (next_year, mut next_year_div) = from_newer_to_older
+        .next()
+        .ok_or("Error: unable to get devidend")?;
+
+    log::info!("DGR: Annual dividend year: {next_year} annual_div: {next_year_div}");
+    for (year, sum) in from_newer_to_older {
+        log::info!("DGR: Annual dividend year: {year} annual_div: {sum}");
+        average += (next_year_div / sum - 1.0) * 100.0;
+        next_year_div = sum;
+    }
+
+    Ok(average)
 }
 
 #[cfg(test)]
@@ -809,7 +832,19 @@ mod tests {
             ("2023-07-01".to_owned(), 0.5),
             ("2023-11-01".to_owned(), 0.5),
         ];
-        assert_eq!(calculate_dgr(&div_hists, 4), Ok(0.0));
+        assert_eq!(calculate_dgr(&div_hists, "2024"), Ok(0.0));
+
+        let div_hists: Vec<(String, f64)> = vec![
+            ("2023-01-01".to_owned(), 0.5),
+            ("2023-04-01".to_owned(), 0.5),
+            ("2023-07-01".to_owned(), 0.5),
+            ("2023-11-01".to_owned(), 0.5),
+            ("2022-01-01".to_owned(), 0.5),
+            ("2022-04-01".to_owned(), 0.5),
+            ("2022-07-01".to_owned(), 0.5),
+            ("2022-11-01".to_owned(), 0.5),
+        ];
+        assert_eq!(calculate_dgr(&div_hists, "2024"), Ok(0.0));
 
         let div_hists: Vec<(String, f64)> = vec![
             ("2022-01-01".to_owned(), 0.1),
@@ -821,10 +856,9 @@ mod tests {
             ("2023-07-01".to_owned(), 2.0),
             ("2023-11-01".to_owned(), 3.0),
         ];
-        assert_eq!(calculate_dgr(&div_hists, 4), Ok(100.0));
+        assert_eq!(calculate_dgr(&div_hists, "2024"), Ok(100.0));
 
         let div_hists: Vec<(String, f64)> = vec![
-            ("2021-12-01".to_owned(), 0.3475),
             ("2022-03-01".to_owned(), 0.365),
             ("2022-06-01".to_owned(), 0.365),
             ("2022-09-01".to_owned(), 0.365),
@@ -836,9 +870,12 @@ mod tests {
             ("2024-03-01".to_owned(), 0.125),
         ];
 
+        //0.125*3.0+0.365 = 0.74
+        //0.365*4.0 = 1.46
+        // DGR: (0.74/1.46 - 1.0)*100.0 = -49.315068
         assert_eq!(
-            Ok::<f64, &str>(round2(calculate_dgr(&div_hists, 4).unwrap())),
-            Ok(-32.06)
+            Ok::<f64, &str>(round2(calculate_dgr(&div_hists, "2024").unwrap())),
+            Ok(-49.32)
         );
 
         let div_hists: Vec<(String, f64)> = vec![
@@ -848,9 +885,78 @@ mod tests {
         ];
 
         assert_eq!(
-            Ok::<f64, &str>(round2(calculate_dgr(&div_hists, 0).unwrap())),
+            Ok::<f64, &str>(round2(calculate_dgr(&div_hists, "2024").unwrap())),
             Ok(0.0)
         );
+
+        // ABEV as of 28th of March 2024
+        let div_hists: Vec<(String, f64)> = vec![
+            ("1970-01-01".to_owned(), 0.0550986),
+            ("2005-10-14".to_owned(), 0.005492),
+            ("2006-01-05".to_owned(), 0.001936),
+            ("2006-04-10".to_owned(), 0.01076),
+            ("2006-07-10".to_owned(), 0.011932),
+            ("2006-11-09".to_owned(), 0.0121),
+            ("2007-01-08".to_owned(), 0.014732),
+            ("2007-04-12".to_owned(), 0.014256),
+            ("2007-06-18".to_owned(), 0.001926),
+            ("2007-07-09".to_owned(), 0.0068092),
+            ("2007-10-26".to_owned(), 0.0325832),
+            ("2007-12-28".to_owned(), 0.010568),
+            ("2008-05-08".to_owned(), 0.031102),
+            ("2008-06-16".to_owned(), 0.0002936),
+            ("2008-08-13".to_owned(), 0.0296124),
+            ("2008-10-24".to_owned(), 0.0173388),
+            ("2009-02-09".to_owned(), 0.0056056),
+            ("2009-06-08".to_owned(), 0.0069884),
+            ("2009-06-22".to_owned(), 0.0010552),
+            ("2009-08-11".to_owned(), 0.0068696),
+            ("2009-10-14".to_owned(), 0.0046288),
+            ("2009-12-29".to_owned(), 0.0102084),
+            ("2010-04-08".to_owned(), 0.0085252),
+            ("2010-10-25".to_owned(), 0.0191172),
+            ("2010-12-22".to_owned(), 0.0299664),
+            ("2011-03-29".to_owned(), 0.0670016),
+            ("2011-08-12".to_owned(), 0.0314662),
+            ("2011-11-28".to_owned(), 0.0112554),
+            ("2012-04-17".to_owned(), 0.0655808),
+            ("2012-08-03".to_owned(), 0.0117658),
+            ("2012-10-22".to_owned(), 0.0108748),
+            ("2013-01-29".to_owned(), 0.0793902),
+            ("2013-04-05".to_owned(), 0.0079676),
+            ("2014-01-30".to_owned(), 0.065248),
+            ("2014-05-02".to_owned(), 0.057983),
+            ("2014-09-05".to_owned(), 0.026945),
+            ("2014-11-24".to_owned(), 0.092538),
+            ("2015-01-21".to_owned(), 0.048092),
+            ("2015-02-06".to_owned(), 0.037219),
+            ("2015-04-07".to_owned(), 0.027902),
+            ("2015-07-09".to_owned(), 0.032246),
+            ("2015-10-08".to_owned(), 0.035761),
+            ("2016-01-07".to_owned(), 0.038278),
+            ("2016-03-07".to_owned(), 0.032999),
+            ("2016-08-05".to_owned(), 0.039616),
+            ("2016-12-05".to_owned(), 0.047804),
+            ("2017-01-05".to_owned(), 0.067134),
+            ("2017-03-02".to_owned(), 0.022606),
+            ("2017-07-24".to_owned(), 0.049309),
+            ("2018-01-08".to_owned(), 0.093373),
+            ("2018-03-05".to_owned(), 0.021539),
+            ("2018-08-06".to_owned(), 0.042972),
+            ("2019-01-07".to_owned(), 0.081518),
+            ("2020-01-07".to_owned(), 0.120835),
+            ("2021-01-11".to_owned(), 0.078966),
+            ("2021-02-04".to_owned(), 0.01424),
+            ("2022-01-06".to_owned(), 0.107707),
+            ("2023-01-05".to_owned(), 0.144287),
+            ("2024-01-08".to_owned(), 0.150969),
+        ];
+
+        assert_eq!(
+            Ok::<f64, &str>(round2(calculate_dgr(&div_hists, "2024").unwrap())),
+            Ok(0.0)
+        );
+
         Ok(())
     }
 
